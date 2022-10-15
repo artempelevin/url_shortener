fda1adf5386c3e1b60acedce5b921cd9120d323f
artempelevin <artempelevin@list.ru>
2022-10-15 19:16:24 +0300

    creating some project structure

---
+++ app/config/__init__.py
@@ -0,0 +1,2 @@
+from app.config.public_config import *
+from app.config.private_config import *
---
+++ app/models/__init__.py
@@ -0,0 +1,4 @@
+from app.models.urls import *
+from app.db import Base
+
+InitializedBase = Base  # Guarantee the load of all metadata

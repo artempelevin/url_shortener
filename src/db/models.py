492044c82bfff867ec77c69666b5b4f1f36ff6ee
artempelevin <artempelevin@list.ru>
2022-10-15 12:46:10 +0300

    add 'visits' and update 'urls' tables

--- src/db/models.py
+++ src/db/models.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, Text, DateTime, String
+from sqlalchemy import Column, Integer, Text, DateTime, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -10,8 +11,20 @@ class Url(Base):
     __tablename__ = 'urls'
 
     id = Column(Integer(), primary_key=True, autoincrement=True)
-    target_url = Column(Text())
-    key = Column(String(length=10))
-    secret_key = Column(String(length=15))
-    created_at = Column(DateTime(), default=datetime.now())
-    updated_at = Column(DateTime(), default=datetime.now(), onupdate=datetime.now())
+    target_url = Column(Text(), nullable=False)
+    key = Column(String(length=10), unique=True, nullable=False)
+    secret_key = Column(String(length=15), unique=True, nullable=False)
+    visits = relationship('Visit')
+    created_at = Column(DateTime(), nullable=False, default=datetime.now())
+    updated_at = Column(DateTime(), nullable=False, default=datetime.now(), onupdate=datetime.now())
+
+
+class Visit(Base):
+    __tablename__ = 'visits'
+
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    url_id = Column(Integer(), ForeignKey('urls.id'))
+    url = relationship('Url')
+    ip = Column(String(length=15), nullable=False)
+    user_agent = Column(Text())
+    visit_time = Column(DateTime(), nullable=False, default=datetime.now())

4f2bf56306fb70109269f677f83d9f1c028936ce
artempelevin <artempelevin@list.ru>
2022-10-15 19:18:07 +0300

    move 'models/'

--- app/models/urls.py
+++ app/models/urls.py
@@ -1,10 +1,9 @@
 from datetime import datetime
 
 from sqlalchemy import Column, Integer, Text, DateTime, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-Base = declarative_base()
+from app.db import Base
 
 
 class Url(Base):
@@ -14,7 +13,7 @@ class Url(Base):
     target_url = Column(Text(), nullable=False)
     key = Column(String(length=10), unique=True, nullable=False)
     secret_key = Column(String(length=15), unique=True, nullable=False)
-    visits = relationship('Visit')
+    visits = relationship('Visit', back_populates='url')
     created_at = Column(DateTime(), nullable=False, default=datetime.now())
     updated_at = Column(DateTime(), nullable=False, default=datetime.now(), onupdate=datetime.now())
 
@@ -24,7 +23,7 @@ class Visit(Base):
 
     id = Column(Integer(), primary_key=True, autoincrement=True)
     url_id = Column(Integer(), ForeignKey('urls.id'))
-    url = relationship('Url')
+    url = relationship('Url', back_populates='visits')
     ip = Column(String(length=15), nullable=False)
     user_agent = Column(Text())
     visit_time = Column(DateTime(), nullable=False, default=datetime.now())
